fe06def9ff7e2155c81904d413b838e3f67d91dd
Kenji Saito <ken-yo@mbr.nifty.com>
2026-03-13 18:58:26 +0900

    Bumps node modules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,36 +1,59 @@
 import { defineConfig } from 'eslint/config';
-import { configs, parser } from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import solid from 'eslint-plugin-solid';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import { parser, configs } from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
 
+import solid from "eslint-plugin-solid/configs/typescript";
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
 export default defineConfig(
+  includeIgnoreFile(gitignorePath),
   {
     ignores: [
+      '**/*.d.ts',
+      '*.{js,jsx}',
+      'src/tsconfig.json',
+      'src/stories',
+      '**/*.css',
+      'node_modules/**/*',
       'dist',
-      'bun.lock',
-    ]
+    ],
   },
+  eslint.configs.recommended,
   ...configs.strict,
   ...configs.stylistic,
-  eslintConfigPrettier,
   {
     files: ['src/**/*.{ts,tsx}'],
     ...solid,
-    plugins: {
-      import: importPlugin,
-    },
     extends: [
       importPlugin.flatConfigs.recommended,
       importPlugin.flatConfigs.typescript,
     ],
     languageOptions: {
       parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parserOptions: {
-        project: "tsconfig.json",
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      '@stylistic': stylistic,
+    },
     settings: {
+      'import/parsers': {
+        espree: ['.js', '.cjs', '.mjs'],
+        '@typescript-eslint/parser': ['.ts'],
+      },
       'import/internal-regex': '^~/',
       'import/resolver': {
         node: {
@@ -42,10 +65,13 @@ export default defineConfig(
       },
     },
     rules: {
-      'react/display-name': 'off',
-      'import/namespace': 'off',
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/quotes': ['error', 'single'],
       'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-    },
+      'import/export': 'off',
+      '@typescript-eslint/unified-signatures': 'off',
+    }
   },
 );
