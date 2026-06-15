6f5c8aefcc09522259e20a3a4d45aaab619cef50
Kenji Saito <ken-yo@mbr.nifty.com>
2026-06-15 22:59:28 +0900

    Bumps node modules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'eslint/config';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import { parser, configs } from 'typescript-eslint';
@@ -6,7 +6,6 @@ import importPlugin from 'eslint-plugin-import';
 
 import solid from "eslint-plugin-solid/configs/typescript";
 
-import { includeIgnoreFile } from '@eslint/compat';
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
